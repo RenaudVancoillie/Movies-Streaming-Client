929832ce581e1431d05230f0744ed4a2adac92f2
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-07 14:49:52 +0200

    Add Movie Interface

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { MoviesComponent } from './movies/movies.component';
+import { MoviesComponent } from './components/movies/movies.component';
 
 @NgModule({
   declarations: [
---
+++ src/app/interfaces/movie.ts
@@ -0,0 +1,12 @@
+export interface Movie {
+    id: number;
+    imdbId: string;
+    title: string;
+    coverUrl: string;
+    year: number;
+    originalAirDate: string;
+    kind: string;
+    rating: number;
+    plot: string;
+    top250Rank: number;
+}
