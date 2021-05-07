7ba0e26c2c8ce9966aed1216b732fa1d4c7037a9
renaud.vancoillie <renaud.vancoillie@student.howest.be>
2021-05-07 15:04:18 +0200

    Use Movie Interface

--- src/app/components/movies/movies.component.ts
+++ src/app/components/movies/movies.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Movie } from '../../interfaces/movie';
+
 @Component({
   selector: 'app-movies',
   templateUrl: './movies.component.html',
@@ -7,7 +9,18 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MoviesComponent implements OnInit {
 
-  movie = 'The Shawshank Redemption';
+  movie: Movie = {
+    id: 1,
+    imdbId: '0111161',
+    title: 'The Shawshank Redemption',
+    coverUrl: 'The Shawshank Redemption.jpg',
+    year: 1994,
+    originalAirDate: '23 Sep 1994 (Mexico)',
+    kind: 'movie',
+    rating: 93.00,
+    plot: null,
+    top250Rank: 1
+  };
 
   constructor() { }
 
